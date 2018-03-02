9e14c741dc6d7f921931c2243395538185d1e5da
eivaskevicius <e.ivaskevicius@iplabs.de>
2018-03-02 11:51:38 +0100

    init

---
+++ src/scripts/makeSnapshot.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e -u -x
+
+git config --global user.email "e.ivaskevicius@iplabs.de"
+git config --global user.name "eivaskevicius"
+git clone resource-sbalpi-dev snapshot
+cp -r node_modules snapshot
+cd snapshot
+
+npm remove jest
+rm -r src/__test__
+
+npm pack
+
+ls -la
+
+git add -A
+git status
+git commit -m "add snapshot"
+git pull
